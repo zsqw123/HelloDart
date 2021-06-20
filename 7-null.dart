239fba5ca80fbaed2b13b5e8f74e1fe863e3acba
damyjy <zsqw123@vip.qq.com>
2021-06-20 23:39:07 +0800

    - add T null

--- 7-null.dart
+++ 7-null.dart
@@ -1,3 +1,15 @@
+import '4-class.dart';
+
 main(List<String> args) {
-  
+  testNull(Point? p) => print(p?.x.bitLength);
+  testNull(Point());
+  Point? p;
+  p ??= Point();
+  p = p ?? Point();
+  p!.x;
+}
+
+class LazyTest {
+  late String awa;
+  static late final LazyTest lazy = LazyTest();
 }
---
+++ 8-other.dart
@@ -0,0 +1,42 @@
+import 'dart:html';
+
+typedef AnimalFunc(Animal animal);
+main(List<String> args) {
+  var strings = <String>[];
+  print(strings is List<String>);
+
+  final animals = <Animal>[];
+  final cats = [Cat(), Cat(), Cat()];
+  animals.addAll(cats);
+
+  TestFun fun = TestFun();
+  print(fun.testAnimal is AnimalFunc);
+  print(fun.testCat is AnimalFunc);
+  print(fun.testDog is AnimalFunc);
+}
+
+class TestFun {
+  testCat(covariant Cat cat) {}
+  testDog(Dog dog) {}
+  testAnimal(Animal animal) {}
+}
+
+class Animal {}
+
+class Cat extends Animal {}
+
+class Dog extends Animal {}
+
+abstract class Feed {
+  feed(Animal animal);
+}
+
+class FeedCat implements Feed {
+  @override
+  feed(covariant Cat animal) => print('feed cat!');
+}
+
+class FeedDog implements Feed {
+  @override
+  feed(Animal animal) => print('dog no food beacuse it not covariant');
+}
